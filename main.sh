4cb73291acd4b5c67b8333810b087bc063e5b2e2
RapiIlham <162597854+RapiIlham@users.noreply.github.com>
2024-03-09 00:51:09 -0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
 while true
 do
-    iptables -t nat -A PREROUTING -p tcp -i eth0 --dport 5307 \ -j REDIRECT --to-ports 127.0.0.1:5307
+    adb reverse 127.0.0.1:5307 0.0.0.0:5307
     netstat -ano
     sleep 128717
 done
