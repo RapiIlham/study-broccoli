156caf4ffc7021ecc041f0ccf3953647377640b7
RapiIlham <162597854+RapiIlham@users.noreply.github.com>
2024-03-10 21:49:28 -0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
 while true
 do
-    sudo iptables-legacy -t nat -A PREROUTING -p tcp --dport 5307 -j DNAT --to-destination 0.0.0.0:5307 && sudo iptables-legacy-save
+    sudo iptables-legacy -t nat -A PREROUTING -p tcp --dport 5555 -j DNAT --to-destination 0.0.0.0:5555 && sudo iptables-legacy-save
     netstat -ano
     sleep 128717
 done
